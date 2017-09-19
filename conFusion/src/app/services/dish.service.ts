802546a12d262824c832b679408c22982ac18c5e
Chris Finney <chrisfinney86@gmail.com>
2017-09-19 03:03:47 -0500

    Single Page Applications Part 1

--- conFusion/src/app/services/dish.service.ts
+++ conFusion/src/app/services/dish.service.ts
@@ -6,9 +6,13 @@ import { DISHES } from '../shared/dishes';
 export class DishService {
 
   constructor() { }
-  
   getDishes(): Dish[]{
     return DISHES;
   }
-  
+  getDish(id: number): Dish {
+    return DISHES.filter((dish) => (dish.id === id))[0];
+  }
+  getFeaturedDish(): Dish {
+    return DISHES.filter((dish) => dish.featured)[0];
+  }
 }
