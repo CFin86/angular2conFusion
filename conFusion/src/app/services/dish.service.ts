54bd104012908ee442fa105565757d518f397638
Chris Finney <chrisfinney86@gmail.com>
2017-09-28 10:46:32 -0500

    promises part 2

--- conFusion/src/app/services/dish.service.ts
+++ conFusion/src/app/services/dish.service.ts
@@ -1,18 +1,30 @@
-import { Injectable } from '@angular/core';
-import { Dish } from '../shared/dish';
-import { DISHES } from '../shared/dishes';
+import {
+  Injectable
+} from '@angular/core';
+import {
+  Dish
+} from '../shared/dish';
+import {
+  DISHES
+} from '../shared/dishes';
 
 @Injectable()
 export class DishService {
 
-  constructor() { }
-  getDishes(): Promise<Dish[]> {
-    return Promise.resolve(DISHES);
+  constructor() {}
+  getDishes(): Promise < Dish[] > {
+    return new Promise(resolve => {
+      setTimeout(() => resolve(DISHES), 2000);
+    })
   }
-  getDish(id: number): Promise<Dish> {
-    return Promise.resolve(DISHES.filter((dish) => (dish.id === id))[0]);
+  getDish(id: number): Promise < Dish > {
+    return new Promise(resolve => {
+      setTimeout(() => resolve(DISHES.filter((dish) => (dish.id === id))[0]), 2000);
+    })
   }
-  getFeaturedDish(): Promise<Dish> {
-    return Promise.resolve(DISHES.filter((dish) => dish.featured)[0]);
+  getFeaturedDish(): Promise < Dish > {
+    return new Promise(resolve => {
+      setTimeout(() => resolve(DISHES.filter((dish) => dish.featured)[0]), 2000);
+    });
   }
 }
